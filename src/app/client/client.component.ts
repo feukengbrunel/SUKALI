719edece85155205c1a24fe8860543aca245c64a
feukengbrunel <feukengbrunel555@gmail.com>
2023-11-10 23:56:37 +0100

    client component

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { 
+    Component ,
+    OnInit,
+    ViewEncapsulation,
+    AfterViewInit,
+  }from '@angular/core';
 
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
-  styleUrls: ['./client.component.css']
+  styleUrls: ['./client.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class ClientComponent {
-
+export class ClientComponent implements OnInit,AfterViewInit{
+   
+ngAfterViewInit(): void {
+  
+}
+ngOnInit(): void {
+  
+}
+img_cart=''
 }
